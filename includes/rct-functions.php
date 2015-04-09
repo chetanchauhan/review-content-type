c45856b413b5fbc54f99ef12c96d76aa0b6762d5
Chetan Chauhan <chetanchauhan1991@gmail.com>
2015-04-09 22:37:08 +0530

    Adding placeholder image(credit woocommerce)

--- includes/rct-functions.php
+++ includes/rct-functions.php
@@ -3,3 +3,21 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
+/**
+ * Retrieve URL for the placeholder image.
+ *
+ * @since   1.0.0
+ * @return string
+ */
+function rct_get_placeholder_image_url() {
+
+	/**
+	 * Filter the placeholder image URL.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $url URL for the placeholder image.
+	 */
+	return apply_filters( 'rct_placeholder_image_url', RCT_URL . 'assets/images/placeholder.png' );
+}
