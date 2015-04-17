32bd8dc853df4f7d81111aaccd23aada133dc1ae
Chetan Chauhan <chetanchauhan1991@gmail.com>
2015-04-17 23:54:01 +0530

    Wrap featured image into another div element on single review page

--- templates/content-single-review-featured-image.php
+++ templates/content-single-review-featured-image.php
@@ -1,13 +1,15 @@
-<div class="rct-featured-image">
-	<?php
-	do_action( 'rct_before_featured_image' );
+<div class="rct-featured-image-wrap">
+	<?php do_action( 'rct_before_featured_image' ); ?>
 
-	if ( $url = rct_get_featured_image_url() ) {
-		printf( '<a href="%s" title="%s" rel="nofollow">%s</a>', esc_url( $url ), esc_attr( rct_get_reviewed_item_name() ), rct_get_featured_image() );
-	} else {
-		echo rct_get_featured_image();
-	}
+	<div class="rct-featured-image">
+		<?php
+		if ( $url = rct_get_featured_image_url() ) {
+			printf( '<a href="%s" title="%s" rel="nofollow">%s</a>', esc_url( $url ), esc_attr( rct_get_reviewed_item_name() ), rct_get_featured_image() );
+		} else {
+			echo rct_get_featured_image();
+		}
+		?>
+	</div>
 
-	do_action( 'rct_after_featured_image' );
-	?>
+	<?php do_action( 'rct_after_featured_image' ); ?>
 </div>
