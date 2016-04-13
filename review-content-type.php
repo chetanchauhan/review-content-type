2e7c8d4fc8315958302ac8f596a9efe7e557369e
Chetan Chauhan <chetanchauhan1991@gmail.com>
2016-04-13 20:14:02 +0530

    Update copyright year

--- review-content-type.php
+++ review-content-type.php
@@ -11,7 +11,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  *
- * Copyright (c) 2015 Chetan Chauhan (email : chetanchauhan1991@gmail.com)
+ * Copyright (c) 2015-2016 Chetan Chauhan (email : chetanchauhan1991@gmail.com)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
