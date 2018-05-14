5b6b79b08cbdc407d8c05118a4f66ce31a9305fb
Chetan Chauhan <chetanchauhan1991@gmail.com>
2018-05-14 10:58:09 +0530

    Allow hardcoded string to be translated

--- templates/content-single-review-price.php
+++ templates/content-single-review-price.php
@@ -4,7 +4,7 @@ $max_price = rct_get_max_price();
 if ( ! rct_is_empty( $max_price ) && ! rct_is_empty( $min_price ) ) { ?>
 
 	<div class="rct-review-price" itemprop="offers" itemscope itemtype="http://schema.org/AggregateOffer">
-		<span><?php echo rct_format_price_amount( $min_price ); ?></span> to
+		<span><?php echo rct_format_price_amount( $min_price ); ?></span> <?php esc_html_e( 'to', 'review-content-type' ); ?>
 		<span><?php echo rct_format_price_amount( $max_price ); ?></span>
 		<meta itemprop="lowPrice" content="<?php echo esc_attr( $min_price ); ?>">
 		<meta itemprop="highPrice" content="<?php echo esc_attr( $max_price ); ?>">
